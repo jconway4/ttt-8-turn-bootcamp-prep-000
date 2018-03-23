d1217dca7bd413a05f835d9a562b45f3b6994851
jconway4 <jconwa4@illinois.edu>
2018-03-23 06:52:31 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,46 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(number)
+  number = number.to_i - 1
+end
+
+def move(array, index, character = 'X')
+  array[index] = character
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == true
+    return false
+  end
+  if position_taken?(board, index) == false
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if (board[index] == "X" || board[index] == "O") && (index.between?(0, 8))
+    return true
+  end
+  if board[index] == " " || board[index] == "" || board[index] == nil && (index.between?(0, 8))
+    return false
+  end
+end
+
+def turn
+  display_board(board)
+  puts "Input move"
+  number = gets
+  index = input_to_index(number)
+  if valid_move?(board, index) == false
+    turn(board)
+  else
+    move(board, index)
+    display_board
+  end
+end
